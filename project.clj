0b24a2881bb76db72c050ba9d6eb8e2949ca73be
Aria Haghighi <me@aria42.com>
2013-02-07 23:50:15 -0800

    added project

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject prismatic/cljs-test "0.0.1"
+  :description "Very simple cljs testing using PhantomJS or Google Closure Test Runners in the browser"
+  :url "https://github.com/prismatic/cljs-test"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:url "git@github.com:prismatic/cljs-test.git"}
+  :clojurescript? true
+  :pom-addition [:developers [:developer
+                              [:name "Prismatic"]
+                              [:url "http://getprismatic.com"]
+                              [:email "admin+oss@getprismatic.com"]
+                              [:timezone "-8"]]]
+  :cljsbuild 
+    {:builds 
+     {:test {:source-paths ["src" "test"]
+             :compiler {:output-to "target/unit-test.js"
+                        :optimizations :whitespace
+                        :pretty-print true}}}
+     :test-commands {"unit" ["phantomjs" "target/unit-test.js"]}}
+  :plugins [[lein-cljsbuild "0.3.0"]])
